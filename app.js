9f1a5d4bcc62a6ff51980ffc2fd706644723863e
suthirakprom <45727524+suthirakprom@users.noreply.github.com>
2019-02-27 20:32:15 +0700

    adding cmt for my own understanding

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ new Vue({
     data: {
         name: 'Shaun',
         job: 'Ninja',
-        website: 'http://www.thenetninja.co.uk',
+        website: 'http://www.thenetninja.co.uk',                //and also the website must has https in front in order to work
         websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
     },
-    methods: {
+    methods: {                              //it has to always be 'methods'
         greet: function(time){
             return 'Good ' + time + ', ' + this.name;
         }
